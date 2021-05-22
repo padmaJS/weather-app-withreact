5a4f19cefc67fe4854403395b525f0772ae37a46
padmaJS <padshak230@gmail.com>
2021-05-22 11:28:53 +0545

    1 file modified

--- src/components/weatherAPI.jsx
+++ src/components/weatherAPI.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 
 class WeatherAPI extends Component {
+  state = {
+    dataLoaded : false,
+    items : null
+  }
+  weatherDataGetter(){
+    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4a0eee4b5e6bc6d8cbb956e511528de&units=metric')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ dataLoaded : true, items : json})
+      })
+  }
   render(){
     return(
-      <h1>hi2</h1> 
+      let {dataLoaded, items} = this.state;
+      <p>{items}{dataLoaded}Hi</p> 
     );
   }
 }
