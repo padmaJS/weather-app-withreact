31b9d6d2d957deca70314e3a92ae0c816475778e
padmaJS <padshak230@gmail.com>
2021-05-22 15:00:24 +0545

    1 file modified

--- src/components/weatherAPI.jsx
+++ src/components/weatherAPI.jsx
@@ -6,7 +6,7 @@ class WeatherAPI extends Component {
     items : null
   }
   weatherDataGetter(){
-    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4a0eee4b5e6bc6d8cbb956e511528de&units=metric')
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=f4a0eee4b5e6bc6d8cbb956e511528de&units=metric')
       .then(response => response.json())
       .then(json => {
         this.setState({ dataLoaded : true, items : json})
